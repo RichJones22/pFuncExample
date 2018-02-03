fd414ec2dda78f27eee3dde0a378daf7f591459a
Rich Jones <jones_rich@yahoo.com>
2018-02-03 14:00:51 -0500

    testing

--- purgeme.sh
+++ purgeme.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # the below three steps will produce those git files that are new or modified, only.
-# - get a list of git files that have been added, deleted, or modified.
+# - get a list of git files that have been added, deleted, or modified with respect to the HEAD
 # - only show those lines that do not begin with a 'D'.
 # - cut or remove the first three characters of the line.
 files=$(git diff HEAD --name-status | grep -v ^D | cut -c3-)
-
 
 # check for version string in each file
 for f in $files
@@ -32,16 +31,16 @@ do
         echo $f | grep -qE '\.(c|cpp)$'
         if [ $? -eq 0 ]
         then
-            # derive date time string
+            # derive date time string as '03FEB2018_111631' format
             current_date_time="`date '+%d%h%Y_%H%M%S' | tr [:lower:] [:upper:]`";
 
-            # derive file name
+            # derive file name with .c extension removed.
             file=${f%.c}
 
-            # rewrite version string
+            # rewrite version string as 'void VERSION_main_03FEB2018_111631_5603(void) {}' format
             sed -i.bak "s/void VERSION_.*/void VERSION_$(echo "${file##*/}")_$(echo "$current_date_time")_$(echo "$RANDOM")(void) {}/g" $f
 
-            # since we just touched the file, we need to 'git add' it again.
+            # since we just updated the file with the previous sed command, we need to 'git add' it again.
             git add $f
 
             # remove temporary .bak file
